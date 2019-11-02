4140e867558d6a2f797c51061e23f735fc6bb224
gerat <gerat@192.168.1.23>
2019-11-02 09:57:26 -0600

    Primer Commit

---
+++ SpringDemoProject/src/test/java/com/progra/demoproject/SpringDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progra.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
